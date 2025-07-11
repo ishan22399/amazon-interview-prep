817cb88d01dfee55ec782f680fa0e194564b7108
Ishan Shivankar <40685427+ishan22399@users.noreply.github.com>
2025-07-11 18:29:24 +0530

    Learning

---
+++ amazon-sde-questions-daily/Learning/OOP/class_&_objects/Car.java
@@ -0,0 +1,41 @@
+public class Car {
+    int noOfWheels;
+    String color;
+    String model;
+    float speed;
+    int fuel;
+    int currentFuelInLiters; 
+    int maxFuelCapacityInLiters;
+
+    public Car(int noOfWheels, String color, String model, float speed, int maxFuelCapacityInLiters) {
+        this.noOfWheels = noOfWheels;
+        this.color = color;
+        this.model = model;
+        this.speed = speed;
+        this.maxFuelCapacityInLiters = maxFuelCapacityInLiters;
+        this.currentFuelInLiters = 0; // Initially, the car has no fuel
+    }
+
+    public void drive() {
+        if(this.currentFuelInLiters <= 0) {
+            System.out.println("Cannot drive. The car has no fuel.");
+            System.out.println("Please refuel the car.");
+            return;
+        }
+
+        System.out.println("The car is driving at " + this.speed + " Km/h.");
+        this.currentFuelInLiters--; // Decrease fuel by 1 liter for each drive
+    }
+
+    public void refuel(int fuel){
+        this.currentFuelInLiters += fuel;
+        if (this.currentFuelInLiters == this.maxFuelCapacityInLiters){
+            System.out.println("The car is now fully refueled.");
+        } else if (this.currentFuelInLiters > this.maxFuelCapacityInLiters){
+            System.out.println("The car cannot hold that much fuel. Excess fuel will be ignored.");
+            this.currentFuelInLiters = this.maxFuelCapacityInLiters; // Cap the fuel to max capacity
+        } else {
+            System.out.println("The car has been refueled. Current fuel: " + this.currentFuelInLiters + " liters.");
+        }
+    }
+}
